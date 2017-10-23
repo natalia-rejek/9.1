ee48690ec4e4b7b3e46f520c52c89a5dd96c3541
Natalia <nettel@wp.pl>
2017-10-23 13:21:23 +0200

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// scripts.js
+
+var 	a,
+	h,
+	triangleArea = getTriangleArea(10,15),
+	triangle1Area = getTriangleArea(12, 15),
+	triangle2Area = getTriangleArea(1, 15);
+
+function getTriangleArea(a, h) {
+
+	if ((a <= 0) || (h <= 0)) {
+    		console.log('Nieprawid³owe dane');
+	}
+	return a*h/2;
+}
+
+
+console.log(getTriangleArea(10, 6));
+console.log(triangleArea);
+console.log(triangle1Area);
+console.log(triangle2Area);
